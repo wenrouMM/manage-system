a775f553fe15590aba1b5b5dd303d1c7ed05f859
张亦凡 <1621289694@qq.com>
2019-05-25 11:02:27 +0800

    5/24

--- src/components/Book/purchasingManagement.vue
+++ src/components/Book/purchasingManagement.vue
@@ -86,6 +86,37 @@
         </section>
       </section>
     </div>
+    <!-- 新增弹框 -->
+    <div class="addEditDialog">
+      <!-- Form -->
+      <el-dialog @close="closeForm" width="500px" :title="Dialogtitle[i]" :visible.sync="dialogFormVisible">
+        <el-form :rules="rules" :model="addForm" id="purchasingMessage" :ref="addForm" style="display: flex;flex-direction: column">
+          <!-- 弹框表单按钮  验证失效-->
+          <el-form-item label=" 采购批次 :" prop="PurchaseBatch" label-width="95px" style="">
+            <el-input v-model="addForm.PurchaseBatch" style="width: 340px"></el-input>
+          </el-form-item>
+          <el-form-item label=" 备注信息 :" prop="remarks" label-width="95px">
+            <el-input type="textarea" v-model="addForm.remarks" style="width: 340px" :autosize="{ minRows: 7, maxRows: 7}" resize="none"></el-input>
+          </el-form-item>
+          <el-form-item style="margin:0px auto">
+            <el-button type="primary" @click="submitForm()" style="margin-left: 20px">确定</el-button>
+            <el-button type="info" @click="resetForm()">取消</el-button>
+          </el-form-item>
+        </el-form>
+      </el-dialog>
+    </div>
+    <!--'删除'-->
+    <div class="forbid">
+      <el-dialog :title="Dialogtitle[i]" :visible.sync="centerDialogVisible" width="500px" center>
+        <div class="dialogBody">
+          是否{{Dialogtitle[i]}}?
+        </div>
+        <div slot="footer">
+          <span class="dialogButton true mr_40" @click="submitDialog">确 定</span>
+          <span class="dialogButton cancel" @click="centerDialogVisible = false">取消</span>
+        </div>
+      </el-dialog>
+    </div>
   </div>
 </template>
 
@@ -99,7 +130,25 @@
           searchData:"",
           currentPage: 0
         },
-        tableData:[]
+        rules:{
+          PurchaseBatch:[{ required: true,message: "请输入采购批次", trigger: "blur" }],
+          remarks:[{ required: true,message: "请输入备注信息", trigger: "blur" }],
+        },
+        addForm:{
+          //添加修改的数据
+          PurchaseBatch:'',//采购批次
+          remarks:'',//备注信息
+        },
+        tableData:[],
+        dialogFormVisible: false, // // 新增修改弹框的展示和消失
+        centerDialogVisible: false, // 删除弹框
+        Dialogtitle: ["修改", "新增",'删除'],
+        i: null, // 切换弹框标题
+        tableChecked: [], // 批量选择的的数据
+        currentPage: 1,
+        pageInput: 1,
+        pageSize: 10,
+        total: 0,
       }
     },
     mounted(){
@@ -108,11 +157,18 @@
     methods:{
       //新增按钮
       rechargeBtn(){
-
+        this.i=1;
+        this.dialogFormVisible=true
+      },
+      //修改按钮
+      EditBtn(){
+        this.i=0
+        this.dialogFormVisible=true
       },
       //删除按钮
       drawbackBtn(val){
-
+        this.i=2
+        this.centerDialogVisible=true
       },
       //导出按钮
       deriveBtn(){
@@ -121,7 +177,50 @@
       //搜索
       searchBtn(){
 
-      }
+      },
+      //新增修改弹框的确定按钮
+      submitForm(){
+
+      },
+      //新增修改弹框的取消按钮
+      resetForm(){
+
+      },
+      //删除弹框的确定按钮
+      submitDialog(){
+
+      },
+      //批量选择
+      handleSelectionChange(val) {
+        console.log('全选按钮之后的数据',val);
+        this.tableChecked = val;
+      },
+      //关闭弹窗
+      closeForm(){
+
+      },
+      current_change(currentPage) {
+        //分页查询
+        this.currentPage = currentPage; //点击第几页
+        this.paginationForm.currentPage = currentPage;
+        console.log("保存当前查询", this.paginationForm, this.currentPage);
+        this.searchApi(this.paginationForm); // 这里的分页应该默认提交上次查询的条件
+      },
+      jumpBtn() {
+        // v-mode绑定好像会默认转数据类型
+        let page = Math.ceil(this.total / this.pageSize);
+        page == 0 ? 1 : page;
+        if (this.pageInput > page) {
+          this.pageInput = 1;
+          this.$nextTick(() => {
+            this.$refs.text.value = 1; // hack方法
+            console.log("Vmode绑定值", this.pageInput);
+          });
+        } else {
+          let num = parseInt(this.pageInput);
+          this.current_change(num);
+        }
+      },
     },
   }
 </script>
@@ -133,7 +232,6 @@
   }
   #purchasing{
     width: 1400px;
-    border: 1px solid gray;
     margin: 70px auto 150px;
   }
   .searchBox{
