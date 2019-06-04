88b2a87d1ec557c16c79c59abd2308e4cabf1f47
carrot0822 <a384713827@qq.com>
2019-06-04 10:07:32 +0800

    配置文件已外置

--- static/config.js
+++ static/config.js
@@ -1,5 +1,5 @@
-export const IP = {
+window.glob = {
   url:'http://192.168.2.54:8088/',
   imgurl:'http://192.168.2.54:8090/',
-   fileUrl : 'http://192.168.2.54:8090/'
+  fileUrl : 'http://192.168.2.54:8090/'
 }
