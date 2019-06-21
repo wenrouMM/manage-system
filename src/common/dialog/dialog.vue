b332acf04cb76d05d138d095cb181aa717929c74
carrot0822 <a384713827@qq.com>
2019-06-21 10:23:31 +0800

    sha

--- src/common/dialog/dialog.vue
+++ src/common/dialog/dialog.vue
@@ -1,4 +1,25 @@
 <template>
-    
+    <div id="dialog">
+        <div class="modal">
+            <slot name="header">
+
+            </slot>
+            <slot name="body">
+
+            </slot>
+            <slot name="footer">
+
+            </slot>
+        </div>
+    </div>
 </template>
 
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
